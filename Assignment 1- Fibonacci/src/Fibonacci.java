159e49aa1a75dafbedcc6692e956c5550185d7fc
John Sapp <sappjohn1994@gmail.com>
2025-01-10 12:26:30 -0500

    Fibonacci Class

---
+++ Assignment 1- Fibonacci/src/Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+
+    public static int fibonacciValue(int n) {
+        if(n==0) return n;
+        if(n==1) return n;
+        return fibonacciValue(n-1)+fibonacciValue(n-2);
+    }
+
+    public static void main(String[] args) {
+        int value = 10;
+        System.out.println("The value of the Fibonacci sequence at " + value +" = " + fibonacciValue(value));
+        }
+    }
